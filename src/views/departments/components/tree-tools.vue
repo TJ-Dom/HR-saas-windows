84488e805b3c0acef1e4229fa0ca4128de366ade
TJ <718785161@qq.com>
2021-09-17 00:47:26 +0800

    完成了组织结构的展示开发

---
+++ src/views/departments/components/tree-tools.vue
@@ -0,0 +1,58 @@
+<template>
+  <el-row
+    type="flex"
+    justify="space-between"
+    align="middle"
+    style="height: 40px;width:100%"
+  >
+    <!-- 左侧内容 -->
+    <el-col><span>{{ treeNode.name }}</span></el-col>
+    <!-- 右侧内容 -->
+    <el-col :span="4">
+      <el-row type="flex" justify="end">
+        <!-- 两个内容 -->
+        <el-col>{{ treeNode.manager }}</el-col>
+        <el-col>
+          <!-- 下拉菜单 element -->
+          <el-dropdown>
+            <!-- 默认显示内容 -->
+            <span> 操作<i class="el-icon-arrow-down" /> </span>
+            <el-dropdown-menu slot="dropdown">
+              <!--  下拉子菜单 -->
+              <el-dropdown-item>添加子部门</el-dropdown-item>
+              <el-dropdown-item v-if="!isRoot">编辑部门</el-dropdown-item>
+              <el-dropdown-item v-if="!isRoot">删除部门</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </el-col>
+      </el-row>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  name: 'TreeNode',
+  components: {},
+  props: {
+    treeNode: {
+      type: Object,
+      required: true
+    },
+    isRoot: {
+      type: Boolean,
+      default: false
+    }
+  },
+  // data () {
+  //   return {}
+  // },
+  computed: {},
+  watch: {},
+  // created () {},
+  // mounted () {},
+  methods: {}
+}
+</script>
+
+<style></style>
--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,27 +1,63 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>
-        组织架构
-      </h2>
+      <el-card class="tree-card">
+        <!-- 用了一个行列布局 -->
+        <TreeNode :tree-node="company" :is-root="true" />
+        <!--放置一个属性   这里的props和我们之前学习的父传子 的props没关系-->
+        <el-tree
+          :data="departs"
+          :props="defaultProps"
+          :default-expand-all="true"
+        >
+          <!-- 传入内容 插槽内容 会循环多次 有多少节点 就循环多少次 -->
+          <!-- 作用域插槽 slot-scope="obj" 接收传递给插槽的数据   data 每个节点的数据对象-->
+          <TreeNode slot-scope="{ data }" :tree-node="data" />
+        </el-tree>
+      </el-card>
     </div>
   </div>
 </template>
 
 <script>
+import TreeNode from './components/tree-tools'
+import { getDepartments } from '@/api/departments'
+import { tranListToTreeData } from '@/utils/index'
 export default {
   name: '',
-  components: {},
+  components: {
+    TreeNode
+  },
   props: {},
-  // data () {
-  //   return {}
-  // },
+  data() {
+    return {
+      company: {},
+      departs: [],
+      defaultProps: {
+        label: 'name' // 表示 从这个属性显示内容
+      }
+    }
+  },
   computed: {},
   watch: {},
-  // created () {},
+  created() {
+    this.getDepartments() // 调用自身的方法
+  },
   // mounted () {},
-  methods: {}
+  methods: {
+    async getDepartments() {
+      const result = await getDepartments()
+      console.log(result)
+      this.company = { name: result.companyName, manager: '负责人' }
+      this.departs = tranListToTreeData(result.depts, '')
+    }
+  }
 }
 </script>
 
-<style></style>
+<style scoped>
+.tree-card {
+  padding: 30px 140px;
+  font-size: 14px;
+}
+</style>
